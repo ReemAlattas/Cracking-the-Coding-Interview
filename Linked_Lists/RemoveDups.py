b46ac268d9bb867ab64ae5cd966cd288d32482f6
Reem <ralattas@live.com>
2021-06-09 17:51:23 +0000

    implement removeDups using a set and temp buffer

---
+++ Linked_Lists/RemoveDups.py
@@ -0,0 +1,59 @@
+### Question: Remove duplicates from unsorted linked list.
+
+# Node class
+class Node:
+  
+  # Function to initialize the node object
+  def __init__(self, data):
+    self.data = data  # Assign data
+    self.next = None  # Initialize
+                        # next as null
+  
+# Linked List class
+class LinkedList:
+    
+  # Function to initialize the Linked
+  # List object
+  def __init__(self):
+    self.head = None
+
+  def printList(self):
+    temp = self.head
+    res = ""
+    while (temp):
+      res = res + str(temp.data) + " "
+      temp = temp.next
+    print(res)
+  
+def removeDups(ll):
+  current = ll.head
+  previous = current
+  visited = set()
+
+  while current:
+    if current.data in visited:
+        previous.next = current.next
+    else:
+        visited.add(current.data)
+        previous = current
+    current = current.next
+  return ll
+
+llist = LinkedList()
+ 
+llist.head = Node(1)
+second = Node(2)
+third = Node(2)
+fourth = Node(3)
+
+llist.head.next = second # Link first node with second
+second.next = third # Link second node with the third node
+third.next = fourth
+
+llist.printList()
+
+removeDups(llist)
+
+llist.printList()
+
+### How to solve this if a temporary buffer is not allowed.
